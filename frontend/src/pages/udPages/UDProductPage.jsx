a847d0e272ed3833995e14f45a78b15e892056ba
Malinda <malindafiv56@gmail.com>
2024-04-10 11:55:49 +0530

    basic backend and frontend codes

---
+++ frontend/src/pages/udPages/UDProductPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import CreateProduct from '../../components/CreateProduct'
+
+
+const UDProduct = () => {
+  return (
+    <>
+    <CreateProduct />
+    </>
+  )
+}
+
+export default UDProduct
