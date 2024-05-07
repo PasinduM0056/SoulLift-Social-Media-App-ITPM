b70069b2c6415f0b5f554201598cf2fa6b49e12a
sandithi1117 <110651880+sandithiV@users.noreply.github.com>
2024-05-07 22:42:11 +0530

    Update TermsAndConditions.jsx
    Updated - Terms and Condition page

--- frontend/src/pages/TermsAndConditions.jsx
+++ frontend/src/pages/TermsAndConditions.jsx
@@ -1,10 +1,67 @@
 import React from 'react';
+import {
+  Box,
+  Heading,
+  Text,
+  Accordion,
+  AccordionItem,
+  AccordionButton,
+  AccordionPanel,
+  AccordionIcon,
+} from '@chakra-ui/react';
 
 function TermsAndConditions() {
   return (
-    <div>
-      
-    </div>
+    <Box p="6">
+      <Heading as="h1" size="xl" mb="4">
+        Terms and Conditions
+      </Heading>
+      <Text fontSize="lg" mb="4">
+        Please read these terms and conditions carefully before using our
+        platform.
+      </Text>
+      <Accordion allowToggle>
+        <AccordionItem>
+          <AccordionButton>
+            <Box flex="1" textAlign="left">
+              Section 1
+            </Box>
+            <AccordionIcon />
+          </AccordionButton>
+          <AccordionPanel pb={4}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam.
+          </AccordionPanel>
+        </AccordionItem>
+        <AccordionItem>
+          <AccordionButton>
+            <Box flex="1" textAlign="left">
+              Section 2
+            </Box>
+            <AccordionIcon />
+          </AccordionButton>
+          <AccordionPanel pb={4}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam.
+          </AccordionPanel>
+        </AccordionItem>
+        <AccordionItem>
+          <AccordionButton>
+            <Box flex="1" textAlign="left">
+              Section 3
+            </Box>
+            <AccordionIcon />
+          </AccordionButton>
+          <AccordionPanel pb={4}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam.
+          </AccordionPanel>
+        </AccordionItem>
+      </Accordion>
+    </Box>
   );
 }
 
