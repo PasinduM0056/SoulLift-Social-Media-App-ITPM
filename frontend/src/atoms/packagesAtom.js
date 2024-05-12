dbda053314efeea108a6e32c0e0aad9c1b382a43
Malinda <malindafiv56@gmail.com>
2024-05-12 06:56:59 +0530

    commit

--- frontend/src/atoms/packagesAtom.js
+++
@@ -1,8 +0,0 @@
-import { atom } from "recoil";
-
-const packagesAtom = atom({
-	key: "packagesAtom",
-	default: [],
-});
-
-export default packagesAtom;
--- frontend/src/atoms/productAtom.js
+++
@@ -1,8 +0,0 @@
-import { atom } from "recoil";
-
-const productsAtom = atom({
-	key: "productsAtom",
-	default: [],
-});
-
-export default productsAtom;
